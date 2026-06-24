15a9d06ea580c07a6324ddbe9b7cf4aba8e4149e
faiyaz73 <faiyazansari15901@gmail.com>
2026-06-24 17:29:28 +0530

    address

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
               <h3 className='text-lg font-semibold text-slate-800'>Address</h3>
             </div>
             <p className='mt-4 text-sm leading-6 text-slate-600'>
-              Patna bihar
+              Head Office : 14/48/3 Barf Khana Udayfganj. Lucknow, Up - 226001, india
             </p>
           </div>
 
