9087aec1019ff786e0ee0cbb63ab98569b987f4a
faiyaz73 <faiyazansari15901@gmail.com>
2026-06-24 17:25:15 +0530

    address

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
               <h3 className='text-lg font-semibold text-slate-800'>Address</h3>
             </div>
             <p className='mt-4 text-sm leading-6 text-slate-600'>
-              Patna
+              Patna bihar
             </p>
           </div>
 
