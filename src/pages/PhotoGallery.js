7b58154f38b14e072ef9b94b188f4a321dec3d22
faiyaz73 <faiyazansari15901@gmail.com>
2026-06-30 11:37:48 +0530

    new value

--- src/pages/PhotoGallery.js
+++ src/pages/PhotoGallery.js
@@ -1,38 +1,22 @@
 import image3 from '../../IMAGE/image_3.png';
 import image4 from '../../IMAGE/image_4.png';
 import image5 from '../../IMAGE/image_5.png';
+import image2 from '../../IMAGE/image_2.png';
 
 export const galleryImages = [
+  image2,
+  image3,
+  image4,
+  image5,
+  
+  image2,
   image3,
   image4,
   image5,
 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
-
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
-
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
-
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
-
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
-
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ryUv2pNp98AaCASH2afDtSifvNBxfzf-phMjfH8H1A&s',
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70',
 
   // aur images yahan add karo
 ];
